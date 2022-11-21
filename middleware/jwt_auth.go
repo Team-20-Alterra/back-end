2df23264135ed4886623b64279a34291d8a57df5
syaiful <syaifulmaulana817@gmail.com>
2022-11-21 22:28:09 +0700

    fix update user

--- middleware/jwt_auth.go
+++ middleware/jwt_auth.go
@@ -5,8 +5,6 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
 
 var whitelist []string = make([]string, 5)
@@ -19,18 +17,8 @@ type JwtCustomClaims struct {
 	jwt.StandardClaims
 }
 
-type ConfigJWT struct {
-	SecretJWT       string
-	ExpiresDuration int
-}
 
 func CreateToken(userId int, username string, email string, role string) (string, error) {
-	// claims := jwt.MapClaims{}
-	// claims["userId"] = userId
-	// claims["username"] = username
-	// claims["role"] = role
-	// claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
-	// Set custom claims
 	claims := &JwtCustomClaims{
 		userId,
 		username,
@@ -41,44 +29,6 @@ func CreateToken(userId int, username string, email string, role string) (string
 		},
 	}
 
-	// Create token with claims
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	// Generate encoded token and send it as response.
-	// t, err := token.SignedString([]byte("secret"))
-
-	// token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(constants.SECRET_KEY))
-}
-
-
-
-func (jwtConf *ConfigJWT) Init() middleware.JWTConfig {
-	return middleware.JWTConfig{
-		Claims:     &JwtCustomClaims{},
-		SigningKey: []byte(jwtConf.SecretJWT),
-	}
-}
-
-func GetUser(c echo.Context) *JwtCustomClaims {
-	user := c.Get("user").(*jwt.Token)
-
-	isListed := CheckToken(user.Raw)
-
-	if !isListed {
-		return nil
-	}
-
-	claims := user.Claims.(*JwtCustomClaims)
-	return claims
-}
-
-func CheckToken(token string) bool {
-	for _, tkn := range whitelist {
-		if tkn == token {
-			return true
-		}
-	}
-
-	return false
 }
